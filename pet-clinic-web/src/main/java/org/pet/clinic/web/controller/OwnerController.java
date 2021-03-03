29229461a1a368478ba8e6e0cd54a163c34a5a14
pkumars3 <puneeth.kumars@dxc.com>
2021-03-03 17:10:25 +0530

    list of all owner on owner index page . Closes #11

--- pet-clinic-web/src/main/java/org/pet/clinic/web/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/org/pet/clinic/web/controller/OwnerController.java
@@ -1,13 +1,23 @@
 package org.pet.clinic.web.controller;
 
+import org.pet.clinic.data.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
-	
+
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
-	public String listOfOwners() {
+	public String listOfOwners(Model model) {
+		model.addAttribute("owners", ownerService.findall());
 		return "owner/index";
 	}
 }
