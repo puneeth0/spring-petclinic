e98efefcee0aa20456ee12c984b6391a6b3911db
pkumars3 <puneeth.kumars@dxc.com>
2021-04-13 19:08:36 +0530

    Adding base entity jpa. Closes #26

--- pet-clinic-data/src/main/java/org/pet/clinic/data/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/model/BaseEntity.java
@@ -2,12 +2,19 @@ package org.pet.clinic.data.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable{
-
-	private	Long Id=null;
+	@javax.persistence.Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private	Long Id;
 }
